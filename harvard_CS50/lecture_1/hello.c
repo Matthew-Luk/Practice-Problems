fc706b408923dde7e36a4cbff7dfb34e0f8ca880
Matthew Luk <Matthew.Luk@yahoo.com>
2022-12-09 22:49:32 -0800

    update

--- harvard_CS50/lecture_1/hello.c
+++ harvard_CS50/lecture_1/hello.c
@@ -1,10 +1,9 @@
-#include "cs50.c"
-#include "cs50.h"
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
     string first = get_string("What's your first name? ");
     string last = get_string("What's your last name? ");
-    printf("hello, %s %s\n", first, last);
+    printf("Hello, %s %s!\n", first, last);
 }
