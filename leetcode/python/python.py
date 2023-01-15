afa7404cb5ebdc2ca2cdeb7ba8eb7ee97e0df3a4
Matthew Luk <Matthew.Luk@yahoo.com>
2023-01-14 23:21:28 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -356,3 +356,12 @@ class Solution:
                     if (i * j) % k == 0:
                         count += 1
         return count
+
+# 2427. Number of Common Factors
+class Solution:
+    def commonFactors(self, a, b):
+        count = 0
+        for i in range(1,a+1):
+            if a % i == 0 and b % i == 0:
+                count += 1
+        return count
