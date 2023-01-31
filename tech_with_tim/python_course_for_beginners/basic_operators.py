b89cf387b04b538ba02ebb5dbc4592da86e2c4e6
Matthew Luk <Matthew.Luk@yahoo.com>
2023-01-30 20:51:21 -0800

    first commit

---
+++ tech_with_tim/python_course_for_beginners/basic_operators.py
@@ -0,0 +1,30 @@
+num1 = 45
+num2 = 4
+num3 = num1//num2
+print(int(num1 / num2))
+
+print(64//10)
+
+print(5%2)
+
+print(num3)
+
+print("Pick a number: ")
+num4 = input()
+print("Pick another number: ")
+num5 = input()
+
+SUM = int(num4) + int(num5)
+print(SUM)
+
+x = 5
+print(x)
+
+print("tim" != "hello")
+
+print(2 < 3)
+print("hello" == "helo")
+print("hello" == "Hello")
+print(2 < 3)
+print(2-3+4 > 5)
+print(False == True)
