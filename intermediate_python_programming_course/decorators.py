eefed093c2dd785630b784eb7a8fe5b9349eab42
Matthew Luk <Matthew.Luk@yahoo.com>
2023-02-12 23:09:09 -0800

    first commit

---
+++ intermediate_python_programming_course/decorators.py
@@ -0,0 +1,13 @@
+def start_end_decorator(func):
+
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper
+
+def print_name():
+    print("Matthew")
+
+print_name = start_end_decorator(print_name)
+print_name()
