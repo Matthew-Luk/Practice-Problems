3349b2d800234c980e1a864135d925234c3f79d5
Matthew Luk <Matthew.Luk@yahoo.com>
2023-02-14 19:16:34 -0800

    update

--- intermediate_python_programming_course/decorators.py
+++ intermediate_python_programming_course/decorators.py
@@ -1,13 +1,19 @@
+import functools
+
 def start_end_decorator(func):
 
-    def wrapper():
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         print("Start")
-        func()
+        result = func(*args, **kwargs)
         print("End")
+        return result
     return wrapper
 
-def print_name():
-    print("Matthew")
+@start_end_decorator
+def add5(x):
+    return x + 5
 
-print_name = start_end_decorator(print_name)
-print_name()
+# print_name = start_end_decorator(print_name)
+print(help(add5))
+print(add5.__name__)
