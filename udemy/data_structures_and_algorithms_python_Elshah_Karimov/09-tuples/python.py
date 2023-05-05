6a14f4b9ef07c0e8461654b011714e44aeb9f167
Matthew Luk <Matthew.Luk@yahoo.com>
2023-05-04 22:17:17 -0700

    first commit

---
+++ udemy/data_structures_and_algorithms_python_Elshah_Karimov/09-tuples/python.py
@@ -0,0 +1,24 @@
+newTuple = ("a", "b", "c", "d", "e")
+# newTuple1 = tuple()
+# newTuple2 = ("a",)
+# print(newTuple[:3])
+
+# for i in range(len(newTuple)):
+#     print(newTuple[i])
+
+# print("f" in newTuple)
+# print(newTuple.index("c"))
+
+# def searchTuple(tuple, element):
+#     for i in tuple:
+#         if i == element:
+#             return True
+#     return False
+
+# print(searchTuple(newTuple, "a"))
+
+myTuple = (1,4,3,2,5)
+myTuple1 = (1,2,6,9,8,7)
+
+print(myTuple + myTuple1)
+print(myTuple * 4)
