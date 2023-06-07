8687016c3d82cb502b85893ef6f82e7936bcbbb9
Matthew Luk <Matthew.Luk@yahoo.com>
2023-06-06 21:59:40 -0700

    first commit

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -510,8 +510,15 @@ class Solution:
 
 # 344. Reverse String
 class Solution:
-    def reverseString(self, s) -> None:
-        s.reverse()
+    def reverseString(self, s: List[str]) -> None:
+        left = 0
+        right = len(s) - 1
+        while left <= right:
+            temp = s[right]
+            s[right] = s[left]
+            s[left] = temp
+            left += 1
+            right -= 1
 
 # 1. Two Sum
 class Solution:
