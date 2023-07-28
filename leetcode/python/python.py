f5eaeb60a53ada0bab2d059a500b4f06c654610e
Matthew Luk <Matthew.Luk@yahoo.com>
2023-07-27 22:02:57 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -798,3 +798,18 @@ class Solution:
             else:
                 result.append(str(i))
         return result
+
+# 104. Maximum Depth of Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root):
+        if root == None:
+            return 0
+        else:
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
