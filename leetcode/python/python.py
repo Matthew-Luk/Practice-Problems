32874464e39bede3823a1915a9d50beb6c28930d
Matthew Luk <Matthew.Luk@yahoo.com>
2023-08-01 21:57:03 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -857,3 +857,12 @@ class Solution:
                 runner = runner.next
             runner.next = runner.next.next
         return head
+
+# 2652. Sum Multiples
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        answer = []
+        for i in range(1,n+1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                answer.append(i)
+        return sum(answer)
