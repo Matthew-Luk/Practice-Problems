1730b61ad03344271be1a1e180f7d6d6b6fcb456
Matthew Luk <Matthew.Luk@yahoo.com>
2023-08-04 23:26:53 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -890,3 +890,20 @@ class Solution:
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
         return (arrivalTime + delayedTime) % 24
+
+# 938. Range Sum of BST
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root, low: int, high: int) -> int:
+        if root == None:
+            return 0
+        if low <= root.val <= high:
+            temp = root.val
+        else:
+            temp = 0
+        return temp + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
