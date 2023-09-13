3e5f52bdf7026d7418ae5afdd48e2483ea8a4936
Matthew Luk <Matthew.Luk@yahoo.com>
2023-09-12 22:35:54 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1219,3 +1219,12 @@ class Solution:
             if s[i] == "a" and seen == True:
                 return False
         return True
+
+# 1791. Find Center of Star Graph
+class Solution:
+    def findCenter(self, edges) -> int:
+        for i in range(len(edges)):
+            if edges[i][0] in edges[i+1]:
+                return edges[i][0]
+            elif edges[i][1] in edges[i+1]:
+                return edges[i][1]
