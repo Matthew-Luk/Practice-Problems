3c2f9de917613cd474fd83a24e7aab6d3709aec3
Matthew Luk <Matthew.Luk@yahoo.com>
2023-09-26 20:22:40 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1290,3 +1290,24 @@ class Solution:
         for i in range(len(nums)):
             answer = answer ^ nums[i]
         return answer
+
+# 66. Plus One
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int("".join([str(x) for x in digits]))
+        number = str(number + 1)
+        number = [int(x) for x in number]
+        return number
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+
+        digits.append(0)
+        digits[0] = 1
+        return digits
