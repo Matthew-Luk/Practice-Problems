7bc8cdc01604a854774660eb71c196d4787ccfa4
Matthew Luk <matthew.luk@yahoo.com>
2023-10-09 23:04:36 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1386,3 +1386,23 @@ class Solution:
             if s.count(s[i]) != t.count(s[i]):
                 return False
         return True
+
+# 94. Binary Tree Inorder Traversal
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.answer=[]
+        self.traversal(root)
+        return self.answer
+        
+    def traversal(self, root):
+        if root == None:
+            return
+        self.traversal(root.left)
+        self.answer.append(root.val)
+        self.traversal(root.right)
