b66d0d53057134ba08a94825a08594a3a0edf511
Matthew Luk <matthew.luk@yahoo.com>
2023-11-15 21:58:20 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1754,3 +1754,26 @@ class Solution:
         self.traversal(root.left)
         self.traversal(root.right)
         self.answer.append(root.val)
+
+# 110. Balanced Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def height(self, root):
+        if root == None:
+            return - 1
+        else:
+            return 1 + max(self.height(root.left), self.height(root.right))
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        lh = self.height(root.left)
+        rh = self.height(root.right)
+        if abs(lh - rh) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        return False
