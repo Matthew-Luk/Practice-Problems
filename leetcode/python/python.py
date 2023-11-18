e21e26053cc4d96d84861a86e58c294789cf1311
Matthew Luk <matthew.luk@yahoo.com>
2023-11-17 21:24:24 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1777,3 +1777,27 @@ class Solution:
         if abs(lh - rh) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
             return True
         return False
+
+# 783. Minimum Distance Between BST Nodes
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        arr = []
+        def traversal(root):
+            if root == None:
+                return
+            traversal(root.left)
+            arr.append(root.val)
+            traversal(root.right)
+        traversal(root)
+        
+        min = arr[-1]
+        for i in range(len(arr)-1):
+            if arr[i+1] - arr[i] < min:
+                min = arr[i+1] - arr[i]
+        return min
