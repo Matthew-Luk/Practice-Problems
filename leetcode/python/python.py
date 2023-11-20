80fed214f27adcde147bfa94edcde57404fd376e
Matthew Luk <matthew.luk@yahoo.com>
2023-11-19 20:49:01 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1801,3 +1801,32 @@ class Solution:
             if arr[i+1] - arr[i] < min:
                 min = arr[i+1] - arr[i]
         return min
+
+# 225. Implement Stack using Queues
+from collections import deque
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+        
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        
+    def pop(self) -> int:
+        for i in range(len(self.queue) - 1):
+            self.push(self.queue.popleft())
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[-1]
+        
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+        
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
