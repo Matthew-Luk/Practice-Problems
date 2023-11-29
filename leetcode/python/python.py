cfa35370219493978bd37f257aff8d0e81dc4234
Matthew Luk <matthew.luk@yahoo.com>
2023-11-28 23:01:36 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1885,3 +1885,40 @@ class Solution:
             else:
                 stack.append(int(i))
         return sum(stack)
+
+# 155. Min Stack
+# LIFO
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        if self.stack == []:
+            self.stack.append(val)
+            self.minstack.append(val)
+        else:
+            if val < self.minstack[-1]:
+                self.stack.append(val)
+                self.minstack.append(val)
+            else:
+                self.stack.append(val)
+                self.minstack.append(self.minstack[-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
