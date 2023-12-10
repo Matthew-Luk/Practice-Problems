baf681ff6dbe22606e32f202144c2dd9ddf71bdb
Matthew Luk <matthew.luk@yahoo.com>
2023-12-09 21:10:00 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -1963,3 +1963,28 @@ class Solution:
                     answer += x
             count += 1
         return answer
+
+# 706. Design HashMap
+class MyHashMap:
+
+    def __init__(self):
+        self.map = [None for _ in range(1000001)]
+        
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+        
+    def get(self, key: int) -> int:
+        if self.map[key] != None:
+            return self.map[key]
+        else:
+            return -1
+
+    def remove(self, key: int) -> None:
+        if self.map[key]:
+            self.map[key] = None
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
