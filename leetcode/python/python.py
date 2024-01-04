22ab499451f2843f2f1e28980fee1c159efc9e4c
Matthew Luk <matthew.luk@yahoo.com>
2024-01-03 21:38:08 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2118,3 +2118,19 @@ class Solution:
             if self.isPalindrome(self.numberToBase(n,i)) == False:
                 return False
         return True
+
+# 2697. Lexicographically Smallest Palindrome
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        l = 0
+        r = len(s) -1
+        s = [x for x in s]
+        while l <= r:
+            if s[l] != s[r]:
+                if ord(s[l]) < ord(s[r]):
+                    s[r] = s[l]
+                else:
+                    s[l] = s[r]
+            l += 1
+            r -= 1
+        return "".join(s)
