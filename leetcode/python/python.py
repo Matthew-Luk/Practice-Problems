df86965ef485d7261712afd8396456135ff145e0
Matthew Luk <matthew.luk@yahoo.com>
2024-01-21 18:01:29 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2275,3 +2275,14 @@ class Solution:
             else:
                 answer[1] -= 1
         return answer == [0,0]
+
+# 1475. Final Prices With a Special Discount in a Shop
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        answer = []
+        for i in range(len(prices)-1):
+            for j in range(i+1, len(prices)):
+                if prices[i] >= prices[j]:
+                    prices[i] = prices[i] - prices[j]
+                    break
+        return prices
