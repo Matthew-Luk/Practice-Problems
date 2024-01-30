6b4b9774c0ac9f2b00c70c2bf1b752fcbc6656e0
Matthew Luk <matthew.luk@yahoo.com>
2024-01-29 19:01:30 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2464,3 +2464,10 @@ class Solution:
             elif i > 0:
                 positive += 1
         return max(positive, negative)
+
+# 2154. Keep Multiplying Found Values by Two
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+        return original
