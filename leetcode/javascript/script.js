192d0f394ca367f186917b5ce7aed136d6b8826d
Matthew Luk <matthew.luk@yahoo.com>
2024-02-21 22:41:32 -0800

    update

--- leetcode/javascript/script.js
+++ leetcode/javascript/script.js
@@ -10,3 +10,16 @@ var numIdenticalPairs = function(nums) {
     }
     return counter
 };
+
+// 2894. Divisible and Non-divisible Sums Difference
+var differenceOfSums = function(n, m) {
+    let answer = 0
+    for(i=1;i<n+1;i++){
+        if(i % m == 0){
+            answer = answer - i
+        }else{
+            answer = answer + i
+        }
+    }
+    return answer
+};
