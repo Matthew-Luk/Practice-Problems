730bccc6f6f41220ab8d648113cd4444f366113c
Matthew Luk <matthew.luk@yahoo.com>
2024-02-24 23:37:49 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2727,3 +2727,20 @@ class Solution:
             l += 1
             r -= 1
         return answer
+
+# 500. Keyboard Row
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        alphabet = {"q":1,"w":1,"e":1,"r":1,"t":1,"y":1,"u":1,"i":1,"o":1,"p":1, 
+                    "a":2,"s":2,"d":2,"f":2,"g":2,"h":2,"j":2,"k":2,"l":2,
+                    "z":3,"x":3,"c":3,"v":3,"b":3,"n":3,"m":3}
+
+        answer = []
+        for i in range(len(words)):
+            lower = words[i].lower()
+            temp = []
+            for j in lower:
+                temp.append(alphabet[j])
+            if len(set(temp)) == 1:
+                answer.append(words[i])
+        return answer
