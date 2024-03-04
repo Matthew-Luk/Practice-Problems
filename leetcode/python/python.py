256459196c9b692fccab50bc7b258e3f28c83a7b
Matthew Luk <matthew.luk@yahoo.com>
2024-03-03 22:16:06 -0800

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2788,3 +2788,18 @@ class Solution:
             else:
                 answer -= num[i]
         return answer
+
+# 728. Self Dividing Numbers
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        results = []
+        for i in range(left,right+1):
+            count = 0
+            if str(i).count("0"):
+                continue
+            for j in str(i):
+                if i % int(j) == 0:
+                    count += 1
+                if count == len(str(i)):
+                    results.append(i)
+        return results
