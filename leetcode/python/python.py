cd72b5349a9eab629068bab573524c980518fafa
Matthew Luk <matthew.luk@yahoo.com>
2024-03-17 14:12:00 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -2972,3 +2972,17 @@ class Solution:
             if abs(arr[i] - arr[i-1]) == min:
                 answer.append([arr[i-1],arr[i]])
         return answer
+
+# 2460. Apply Operations to an Array
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] = nums[i] * 2
+                nums[i+1] = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count], nums[i] = nums[i], nums[count]
+                count += 1
+        return nums
