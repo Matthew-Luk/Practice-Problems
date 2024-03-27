b83361200d58320d7cb28338eead0a4e73128794
Matthew Luk <matthew.luk@yahoo.com>
2024-03-26 22:59:37 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -3081,3 +3081,20 @@ class Solution:
             else:
                 count = 0
         return False
+
+# 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        if not original:
+            return None
+        else:
+            if original == target:
+                return cloned
+            return self.getTargetCopy(original.left, cloned.left, target) or self.getTargetCopy(original.right, cloned.right, target)
