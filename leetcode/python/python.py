ade0c7f21008861c49dfe5c3642e5102b23b315b
Matthew Luk <matthew.luk@yahoo.com>
2024-04-03 21:20:20 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -3160,3 +3160,12 @@ class Solution:
             elif sentence[i][0] not in vowels:
                 answer.append(sentence[i][1:] + sentence[i][0] + "ma" + ("a"*(i+1)))
         return " ".join(answer)
+
+# 908. Smallest Range I
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return 0
+        if (max(nums)-k) - (min(nums)+k) > 0:
+            return (max(nums)-k) - (min(nums)+k)
+        return 0
