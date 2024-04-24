cf9f365616683648fb674abb48b45de0bf611d81
Matthew Luk <matthew.luk@yahoo.com>
2024-04-23 21:24:23 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -3439,3 +3439,31 @@ class Solution:
                 s.remove(i)
                 result += i
         return result
+
+# 1304. Find N Unique Integers Sum up to Zero
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        answer = []
+        if n == 1:
+            return [0]
+        elif n % 2 == 0:
+            for i in range(1,(n//2)+1):
+                answer.append(i)
+                answer.append(i*-1)
+            return answer
+        else:
+            for i in range(1,(n//2)+1):
+                answer.append(i)
+                answer.append(i*-1)
+            answer.append(0)
+            return answer
+
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        answer = []
+        for i in range(1,(n//2)+1):
+            answer.append(i)
+            answer.append(i*-1)
+        if n % 2 == 1:
+            answer.append(0)
+        return answer
