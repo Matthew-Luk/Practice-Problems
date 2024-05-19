8715ec8ffb85651f2aef755b2614b85678564645
Matthew Luk <matthew.luk@yahoo.com>
2024-05-18 23:41:01 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -3823,3 +3823,11 @@ class Solution:
             else:
                 count = 1
         return answer
+
+# 1903. Largest Odd Number in String
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num)-1,-1,-1):
+            if int(num[i]) % 2 == 1:
+                return num[:i+1]
+        return ""
