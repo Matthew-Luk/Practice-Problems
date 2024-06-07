831dfd9063b4239f770e3d009473a641836ca032
Matthew Luk <matthew.luk@yahoo.com>
2024-06-06 23:43:03 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -4142,3 +4142,14 @@ class Solution:
             if v == 1:
                 answer.append(k)
         return answer
+
+# 2490. Circular Sentence
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        if sentence[0] != sentence[-1]:
+            return False
+        for i in range(len(sentence)):
+            if sentence[i] == " ":
+                if sentence[i-1] != sentence[i+1]:
+                    return False
+        return True
