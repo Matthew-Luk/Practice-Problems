fd03086f1ed4e49b8c2a549f8e7db2c62afd09fc
Matthew Luk <matthew.luk@yahoo.com>
2024-07-11 21:16:41 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -4375,3 +4375,12 @@ class Solution:
 
         traversal(root)
         return answer
+
+# 1876. Substrings of Size Three with Distinct Characters
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        answer = 0
+        for i in range(len(s)-2):
+            if s[i] != s[i+1] and s[i] != s[i+2] and s[i+1] != s[i+2]:
+                answer += 1
+        return answer
