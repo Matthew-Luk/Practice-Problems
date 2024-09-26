64bb0bad2d363b28c530ecf153d96c639a595237
Matthew Luk <matthew.luk@yahoo.com>
2024-09-25 21:32:35 -0700

    update

--- leetcode/python/python.py
+++ leetcode/python/python.py
@@ -5259,3 +5259,38 @@ class Solution:
 #             if l == r-1:
 #                 if isBadVersion(r) == True and isBadVersion(l) == False:
 #                     return r
+
+# 232. Implement Queue using Stacks
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        x = self.stack2.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return x
+        
+    def peek(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        x = self.stack2[-1]
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return x
+
+    def empty(self) -> bool:
+        return self.stack1 == []
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
